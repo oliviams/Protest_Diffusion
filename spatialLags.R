5ac099501cc65b0a6853b3396b700a24eb0fb23e
Olivia <olivia.macmillan-scott.16@ucl.ac.uk>
2022-11-14 16:53:08 +0000

    corrected renamed columns

--- spatialLags.R
+++ spatialLags.R
@@ -11,33 +11,33 @@ geoID <- dist.matrix[,1]
 		rownames(dist.matrix) <- colnames(dist.matrix) <- as.character(geoID)
       dist.matrix <- dist.matrix[order(rownames(dist.matrix)),order(colnames(dist.matrix))]
 
-protest.data$GEOID10 <- as.character(protest.data$GEOID10)
-		protest.data <- arrange(protest.data,GEOID10,Year.Week)
+protest.data$GEOID10_x <- as.character(protest.data$GEOID10_x)
+		protest.data <- arrange(protest.data,GEOID10_x,Year.Week)
 			protest.data$indicator <- protest.data$Protest.
 
 
 weeks <- unique(protest.data$Year.Week)
 
 output <- data.frame(matrix(NA,nrow=1,ncol=3)) # empty dataframe to be filled with protest data using spatial lag
-	names(output) <- c("GEOID10","sp_protest","Year.Week")
+	names(output) <- c("GEOID10_x","sp_protest","Year.Week")
 
 #Adding spatial lag and populating dataframe (distance matrix * protest occurrence)
 for(i in 1:length(weeks)){
 temp.week <- weeks[i]
 	temp.data <- protest.data[protest.data$Year.Week==temp.week,]
-		temp.ids <- intersect(temp.data$GEOID10,rownames(dist.matrix))
+		temp.ids <- intersect(temp.data$GEOID10_x,rownames(dist.matrix))
 			if(length(temp.ids)>1){
 				temp.matrix <- dist.matrix[temp.ids,temp.ids]
-					temp.value <- temp.data$indicator[which(temp.data$GEOID10 %in% temp.ids)]
+					temp.value <- temp.data$indicator[which(temp.data$GEOID10_x %in% temp.ids)]
 						temp.spatial <- temp.matrix %*% temp.value   # matrix multiplication
 							temp.out <- data.frame(temp.ids,temp.spatial,temp.week)
-								names(temp.out) <- c("GEOID10","sp_protest","Year.Week")
+								names(temp.out) <- c("GEOID10_x","sp_protest","Year.Week")
 									output <- rbind(output,temp.out)
 					}
 	}
 output <- output[-1,]
 
 # Joining spatial lag data to protest data 
-protest.data <- left_join(protest.data,output,by=c("GEOID10"="GEOID10","Year.Week"="Year.Week"))
+protest.data <- left_join(protest.data,output,by=c("GEOID10_x"="GEOID10_x","Year.Week"="Year.Week"))
 
 write.csv(protest.data,file="./data/weekly_data_lagged.csv")
